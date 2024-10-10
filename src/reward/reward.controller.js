be7b2dc562907e821a84a9f90279a1705bd07194
Kalam Azad <kazad79@gmail.com>
2024-10-10 17:39:53 +0800

    new Ref-Fix-TEMP2

--- src/reward/reward.controller.js
+++ src/reward/reward.controller.js
@@ -40,6 +40,8 @@ const  env  = process.env;
         let project_code =req.headers["project_code"];
         let products_ids = env[project_code]
         console.log(products_ids)
+        console.log(env)
+        console.log(project_code)
         if(products_ids){
          
         let array =  ["66e78d6e54ced7123a184796","66e65f1e2f3ef81b47544c76","66e665de966efc2edaa97cf0"];// JSON.parse(products_ids);
